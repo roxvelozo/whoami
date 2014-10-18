22fadaf301ff32b2116dc5eea27b4fbd217cb769
GuiPinto <me@guipinto.com>
2014-10-18 15:00:48 -0400

    Guis last commit! Good luck roxxane!

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(function() {
 
+	var marks = [
+		'',
+		'',
+		''
+	]
+
 	var game = {
 		showPage: function(pageToShow) {
 			// First, lets hide all pages (except pageToShow)
@@ -12,49 +18,57 @@ $(function() {
 
 
 
-	/** Page Specific Logic **/
+	/****
+	***** Page Specific Logic
+	*****/
 	$('#capture').bind('didShow', function(){
 
 	 	$("#captureBtn").trigger('click');
 
-	});
-	$('#position').bind('didShow', function(){
-
+	 	initializeAndSetupPinchAndZoom();
 
 	});
+	$('#flip').bind('didShow', function(){
 
+		// Start timer
+
+		// Update countdown label (div)
+
+		// When timer finishes, trigger game.showPage('game');
+
+	});
+	$('#game').bind('didShow', function(){
+
+		// copy image source
+		// < Through jquery-copy > [comes in with new position AND source?]
+		$("#game .capturedImage").html(''); // Empty it out
+		var originalImage = $("#capture .capturedImage img").clone();
+		originalImage.appendTo($("#game .capturedImage"));
+
+		// Choose random mask.
+
+		// Display Game timer <div>
+
+		// Display Controls <div>
+
+	});
+
+
+
+
+	/****
+	***** General Setup
+	*****/
 
 	$("#capture .retake-btn").on('click', function() {
 		$("#captureBtn").trigger('click');
-
 	});
 
-	/*$("#capture .continue-btn").on('click', function() {
+	$("#capture .start-btn").on('click', function() {
+		game.showPage('game');
+	});
 
-		// setup position screen
-		$("#position .capturedImage img").attr('src',
-			$("#capture .capturedImage img").attr('src')
-		);
-
-		game.showPage('position');
-
-	});*/
-
-
-
-
-
-/*var canvas_width = $('#canvas').width();
-var canvas_height = $('#canvas').height();
-
-$('#canvas').replaceWith('<canvas width="'+ canvas_width +'" height="'+ canvas_height +'"></canvas>')	*/
-
-
-
-
-	/** General  Setup **/
 	var captureBtn = document.getElementById("captureBtn")
-
  	captureBtn.addEventListener("change", function (evt) {
 		var capture = this.files[0];
 
@@ -85,11 +99,127 @@ $('#canvas').replaceWith('<canvas width="'+ canvas_width +'" height="'+ canvas_h
 
 
 
-
-
 	// INITIALIZATION!!
 	// Lets start inside of our capture screen.
 	game.showPage('capture');
+
+
+
+
+
+
+	var pinchAndZoomSetup = false;
+	function initializeAndSetupPinchAndZoom() {
+		if (pinchAndZoomSetup) return; // Only set it up once!
+		pinchAndZoomSetup = true;
+
+		// polyfill
+		var reqAnimationFrame = (function () {
+		    return window[Hammer.prefixed(window, 'requestAnimationFrame')] || function (callback) {
+		        window.setTimeout(callback, 1000 / 60);
+		    };
+		})();
+
+		var screen = $("#capture .capturedImage")[0];
+		var el = $("#capture .capturedImage img")[0];
+
+		var START_X = Math.round((screen.offsetWidth - el.offsetWidth) / 2);
+		var START_Y = Math.round((screen.offsetHeight - el.offsetHeight) / 2);
+
+		var ticking = false;
+		var transform;
+		var timer;
+
+		var mc = new Hammer.Manager(el);
+
+		mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
+
+		mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
+		mc.add(new Hammer.Rotate({ threshold: 0 })).recognizeWith(mc.get('pan'));
+		mc.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith([mc.get('pan'), mc.get('rotate')]);
+
+		mc.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
+		mc.add(new Hammer.Tap());
+
+		mc.on("panstart panmove", onPan);
+		mc.on("pinchstart pinchmove", onPinch);
+		mc.on("swipe", onSwipe);
+
+		function resetElement() {
+		    el.className = 'animate';
+		    transform = {
+		        translate: { x: START_X, y: START_Y },
+		        scale: 1,
+		        angle: 0,
+		        rx: 0,
+		        ry: 0,
+		        rz: 0
+		    };
+		    requestElementUpdate();
+		}
+
+		function updateElementTransform() {
+		    var value = [
+		        'translate3d(' + transform.translate.x + 'px, ' + transform.translate.y + 'px, 0)',
+		        'scale(' + transform.scale + ', ' + transform.scale + ')',
+		        'rotate3d('+ transform.rx +','+ transform.ry +','+ transform.rz +','+  transform.angle + 'deg)'
+		    ];
+
+		    value = value.join(" ");
+		    el.style.webkitTransform = value;
+		    el.style.mozTransform = value;
+		    el.style.transform = value;
+		    ticking = false;
+		}
+
+		function requestElementUpdate() {
+		    if(!ticking) {
+		        reqAnimationFrame(updateElementTransform);
+		        ticking = true;
+		    }
+		}
+
+		function onPan(ev) {
+		    el.className = '';
+		    transform.translate = {
+		        x: START_X + ev.deltaX,
+		        y: START_Y + ev.deltaY
+		    };
+
+		    requestElementUpdate();
+		}
+
+		var initScale = 1;
+		function onPinch(ev) {
+		    if(ev.type == 'pinchstart') {
+		        initScale = transform.scale || 1;
+		    }
+
+		    el.className = '';
+		    transform.scale = initScale * ev.scale;
+
+		    requestElementUpdate();
+		}
+
+		function onSwipe(ev) {
+		    var angle = 50;
+		    transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
+		    transform.rx = (ev.direction & Hammer.DIRECTION_VERTICAL) ? 1 : 0;
+		    transform.angle = (ev.direction & (Hammer.DIRECTION_RIGHT | Hammer.DIRECTION_UP)) ? angle : -angle;
+
+		    clearTimeout(timer);
+		    timer = setTimeout(function () {
+		        resetElement();
+		    }, 300);
+
+		    requestElementUpdate();
+		}
+
+		resetElement();
+
+	}
+
+
 
 
 });
