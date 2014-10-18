3115385e1fd8aba3aac77690e0bc6bc041b8ff67
Roxane Velozo <roxanevelozo@roxvelozo.local>
2014-10-18 12:31:15 -0400

    initial files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+
+/*var canvas_width = $('#canvas').width();
+var canvas_height = $('#canvas').height();
+
+$('#canvas').replaceWith('<canvas width="'+ canvas_width +'" height="'+ canvas_height +'"></canvas>')	*/
+
+});
